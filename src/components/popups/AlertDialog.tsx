1769da2cd32cfa99b63b93d6498f58c37ed1c31b
adityadeshpande0 <adityadeshpande1@outlook.com>
2024-09-16 17:11:40 +0530

    topnavbar added

---
+++ src/components/popups/AlertDialog.tsx
@@ -0,0 +1,6 @@
+
+const AlertDialog: React.FC = () => {
+  return <div>This is Aler dialog box</div>;
+};
+
+export default AlertDialog;
--- src/components/topnavbar/TopNavbar.tsx
+++ src/components/topnavbar/TopNavbar.tsx
@@ -1,6 +1,30 @@
+import React from 'react';
+import './topnav-styles.scss';
+
 const TopNavbar: React.FC = () => {
-    return <>Top Navbar</>;
-  };
-  
-  export default TopNavbar;
-  
+  const navLinks = ["file", "edit", "view", "go", "run", "terminal", "help"];
+
+  return (
+    <div className="mainblock-navbar container-fluid">
+      <div className="row align-items-center">
+        <div className="col-auto">
+          <img alt="vs-code" className="logo-navbar" />
+        </div>
+        <div className="col d-none d-md-flex">
+          <div className="linkblock-navbar">
+            {navLinks.map((link) => (
+              <p key={link} className="linktext-navbar">{link}</p>
+            ))}
+          </div>
+        </div>
+        <div className="col text-center">
+          <div className="titleblock-navbar">
+            <p className="title-navbar">Aditya Deshpande - Portfolio</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default TopNavbar;
