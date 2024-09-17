dd7683c9e3e7cff3bc7a0ed64b02934e0c8cdf88
adityadeshpande0 <adityadeshpande1@outlook.com>
2024-09-17 19:04:50 +0530

    footer developed

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,5 +1,34 @@
+import branchIcon from "../../assets/icons/branch_icon_footer.svg";
+import crossIcon from "../../assets/icons/cross_icon_footer.svg";
+import alertIcon from "../../assets/icons/alert_icon_footer.svg";
+import bellIcon from "../../assets/icons/bell_icon_footer.svg";
+
 const Footer: React.FC = () => {
-  return <>Footer</>;
+  return (
+    <div className="d-flex justify-content-between align-items-center bg-primary">
+      <div className="d-flex align-items-center">
+        <div className="d-flex align-items-center me-4 p-2 hover-bg-secondary">
+          <img alt="branch" src={branchIcon} className="me-2" />
+          <a
+            href="https://github.com/adityadeshpande0/MyPortfolio"
+            className="text-white text-decoration-none"
+          >
+            main
+          </a>
+        </div>
+        <div className="d-flex align-items-center p-2 hover-bg-secondary">
+          <img alt="cross" src={crossIcon} className="me-2" />
+          <p className="text-white mb-0 me-4">0</p>
+          <img alt="alert" src={alertIcon} className="me-2" />
+          <p className="text-white mb-0">0</p>
+        </div>
+      </div>
+      <div className="d-flex align-items-center p-2">
+        <p className="text-white mb-0 me-4">Made in React JS</p>
+        <img alt="bell" src={bellIcon} />
+      </div>
+    </div>
+  );
 };
 
 export default Footer;
--- src/router/AppRouter.tsx
+++ src/router/AppRouter.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "../screens/Home/Home";
 import TopNavbar from "../components/topnavbar/TopNavbar";
 import Projects from "../screens/Projects/Projects";
+import Footer from "../components/footer/Footer";
 
 
 const AppRouter: React.FC = () => {
@@ -12,6 +13,7 @@ const AppRouter: React.FC = () => {
        <Route path="/" Component={Home}/>
        <Route path='/projects' Component={Projects}/>
       </Routes>
+      <Footer/>
     </Router>
   );
 };
