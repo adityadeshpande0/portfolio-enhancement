9bb8ad6efbcdfdf5a9344ab503ad3135aa30c139
adityadeshpande0 <adityadeshpande1@outlook.com>
2024-09-17 18:05:03 +0530

    navbar styles fixes

--- src/components/topnavbar/TopNavbar.tsx
+++ src/components/topnavbar/TopNavbar.tsx
@@ -1,20 +1,25 @@
-import React from 'react';
-import vscode from '../../assets/icons/vs-code-image.svg';
+import React from "react";
+import vscode from "../../assets/icons/vs-code-image.svg";
+import "./topnav-styles.scss";
 
 const TopNavbar: React.FC = () => {
   const navLinks = ["file", "edit", "view", "go", "run", "terminal", "help"];
 
   return (
-    <div className="container-fluid bg-dark">
+    <div className="main-container container-fluid bg-dark">
       <div className="row align-items-center">
         <div className="col-auto d-flex">
-          <img src={vscode} alt="vs-code" className="logo-navbar" style={{ width: '20px', height: '20px', marginRight: '15px', marginLeft: '10px', cursor: 'pointer' }} />
+          <img src={vscode} alt="vs-code" className="logo-navbar" />
           {navLinks.map((link) => (
-            <p key={link} className="mb-0 text-light mx-2" style={{ cursor: 'pointer', fontSize: '13px' }}>{link}</p>
+            <p key={link} className="navlinks mb-0 mx-2">
+              {link}
+            </p>
           ))}
         </div>
         <div className="col text-center">
-          <p className="mb-0 text-light" style={{ fontSize: '13px' }}>Aditya Deshpande - Portfolio</p>
+          <p className="navbartext mb-0 text-light">
+            Aditya Deshpande - Portfolio
+          </p>
         </div>
         <div className="col-auto"></div>
       </div>
